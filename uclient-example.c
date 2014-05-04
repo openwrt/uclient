e034277e8e3bbae9f1dbccf2b21075c0ff84ddb5
Felix Fietkau <nbd@openwrt.org>
2014-05-05 01:12:57 +0200

    example: document -q
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- uclient-example.c
+++ uclient-example.c
@@ -224,6 +224,7 @@ static int usage(const char *progname)
 	fprintf(stderr,
 		"Usage: %s [options] <URL>\n"
 		"Options:\n"
+		"	-q:                             Turn off status messages\n"
 		"	-O <file>:                      Redirect output to file (use \"-\" for stdout)\n"
 		"\n"
 		"HTTPS options:\n"
